4b44cc6685bf6a4a87f18a130e8d2504bb7205db
SirCQQ <gatucristian@gmail.com>
2022-10-08 14:52:57 +0300

    Added husky and lint-parsed

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -5,7 +5,7 @@ import Page from 'components/page';
 export default function index() {
   return (
     <Page>
-      <Text>Works</Text>
+      <Text>This is the page for workds</Text>
     </Page>
   );
 }
